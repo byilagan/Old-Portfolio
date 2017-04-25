6911dbf857013b3b3e9e1cec1981481f440e5a21
Bailey Ilagan <ilaganbgm@gmail.com>
2017-04-24 19:53:19 -0700

    Add files via upload
    Update #2

--- animations/home.js
+++ animations/home.js
@@ -1,5 +1,7 @@
 $(function(){
-    $("#name").css("display","none");
-    $("#name").fadeIn("5000"); 
+    $("html").show();
+    $("#name img").delay("500").fadeIn("slow"); 
+    $("#name h1").delay("1000").fadeIn("slow");
+    $("#name h2").delay("1500").fadeIn("slow");
 });
 
